3869aa79d67b166badb338b1b7e155a7a9a6ea26
Omar Monreal <omarmonros@gmail.com>
2023-11-29 19:16:33 -0800

    se agrego elemento de video

--- app.js
+++ app.js
@@ -4,23 +4,30 @@ let rutas = [
   {
     id: 1,
     lugares: ['Parque industrial Otay', 'Zona Industrial Murua', 'Parque Chilpancingo', 'Ley Murua', 'Central Camionera', '5 y 10'],
+    video: 'https://www.youtube.com/embed/Ma1kJcA70yo?si=4OroZT99UvcsibRG',
     imagen: 'images/murua.png'
   },
   {
     id: 2,
     lugares: ['5 y 10', 'Blvd Insurgentes', 'Macroplaza', 'El Florido 2da Seccion', 'Mariano Matamoros'],
+    video: 'https://www.youtube.com/embed/GquEnoqZAK0?si=1IoMSYYJez9lThdp',
     imagen: 'images/insurgentes-mariano.png'
   },
   {
     id: 3,
     lugares: ['5 y 10', 'Parque Morelos', 'Estadio Chevron', 'Residencial Insurgentes', 'Colonia Capistrano'],
+    video: 'https://www.youtube.com/embed/MHB8KFZT57s?si=owNIAh8jWLuWoDe5',
     imagen: 'images/capistrano.png'
   }
 ];
 
+//
+//
+
 /* VARIABLES */
 const seleccionarRuta = document.getElementById('seleccionar-calafia');
 const lugares = document.querySelector('ol');
+const video = document.getElementById('video');
 const mapa = document.getElementById('img-mapa');
 
 seleccionarRuta.addEventListener('change', buscarInfo);
@@ -35,6 +42,7 @@ function buscarInfo(e) {
 
 function mostrarInfo(ruta) {
   mapa.src = ruta.imagen;
+  video.src = ruta.video;
    
   while (lugares.childElementCount) //limpiamos la lista de lugares
     lugares.removeChild(lugares.firstChild);
